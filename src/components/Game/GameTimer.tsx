8a9efa4c0bc8ade3493fdbb6e01cdf403b8e96f1
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-06-23 17:40:37 +0900

    refactor : 타이머를 useTimer커스텀 훅으로 구현

--- src/components/Game/GameTimer.tsx
+++ src/components/Game/GameTimer.tsx
@@ -1,22 +1,7 @@
-import { gameStatusState, gameTimerState } from "@/atoms/gameAtoms";
-import { useEffect } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
+import useTimer from "@/hooks/useTimer";
 
 export default function GameTimer() {
-  const gameStatus = useRecoilValue(gameStatusState);
-  const [time, setTime] = useRecoilState(gameTimerState);
-  useEffect(() => {
-    let timer = null;
-
-    if (gameStatus === "START") {
-      timer = setInterval(() => {
-        setTime((prev) => prev + 1);
-      }, 1000);
-    }
-    return () => {
-      if (timer) clearInterval(timer);
-    };
-  }, [gameStatus]);
+  const time = useTimer();
 
   return <div className="font-bold">{time}</div>;
 }
---
+++ src/hooks/useTimer.ts
@@ -0,0 +1,21 @@
+import { gameStatusState, gameTimerState } from "@/atoms/gameAtoms";
+import { useEffect } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+
+export default function useTimer() {
+  const gameStatus = useRecoilValue(gameStatusState);
+  const [time, setTime] = useRecoilState(gameTimerState);
+  useEffect(() => {
+    let timer = null;
+
+    if (gameStatus === "START") {
+      timer = setInterval(() => {
+        setTime((prev) => prev + 1);
+      }, 1000);
+    }
+    return () => {
+      if (timer) clearInterval(timer);
+    };
+  }, [gameStatus]);
+  return time;
+}
